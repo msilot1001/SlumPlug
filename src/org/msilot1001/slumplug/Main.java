fb028a4493c39c7bdfbb75dbae0bb3585ac1232e
msilot1001 <msilot1001@gmail.com>
2023-02-08 21:00:40 +0900

    fix

--- src/org/msilot1001/slumplug/Main.java
+++ src/org/msilot1001/slumplug/Main.java
@@ -6,12 +6,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("SLUM PLUGIN ENABLED");
+        Bukkit.getLogger().info("[SLUMPLUG] SLUM PLUGIN ENABLED");
         getServer().getPluginManager().registerEvents(new ButtonEvent(), this);
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getLogger().info("SLUM PLUGIN DISABLED");
+        Bukkit.getLogger().info("[SLUMPLUG] SLUM PLUGIN DISABLED");
     }
 }
